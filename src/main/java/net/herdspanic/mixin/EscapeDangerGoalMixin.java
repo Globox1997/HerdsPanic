d98623b6d47a69c6f6adb2f5e745d2911127341c
Globox1997 <mitschke11@web.de>
2020-12-21 21:22:26 +0100

    initial commit

---
+++ src/main/java/net/herdspanic/mixin/EscapeDangerGoalMixin.java
@@ -0,0 +1,40 @@
+package net.herdspanic.mixin;
+
+import java.util.List;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.At;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.ai.goal.EscapeDangerGoal;
+import net.minecraft.entity.ai.goal.Goal;
+import net.minecraft.entity.mob.PathAwareEntity;
+import net.minecraft.predicate.entity.EntityPredicates;
+
+@Mixin(EscapeDangerGoal.class)
+public abstract class EscapeDangerGoalMixin extends Goal {
+    @Shadow
+    protected final PathAwareEntity mob;
+
+    public EscapeDangerGoalMixin(PathAwareEntity mob, double speed) {
+        this.mob = mob;
+    }
+
+    @Inject(method = "start", at = @At("HEAD"))
+    public void startMixin(CallbackInfo info) {
+        List<Entity> list = mob.world.getEntitiesByClass(PathAwareEntity.class, mob.getBoundingBox().expand(10D),
+                EntityPredicates.EXCEPT_SPECTATOR);
+        for (int i = 0; i < list.size(); ++i) {
+            PathAwareEntity entity = (PathAwareEntity) list.get(i);
+            if (entity.getType() == mob.getType() && !mob.world.isClient) {
+                if (mob.getAttacker() != null && entity.getAttacker() == null && mob.distanceTo(entity) < 16F) {
+                    entity.setAttacker(mob.getAttacker());
+                }
+            }
+        }
+    }
+
+}
